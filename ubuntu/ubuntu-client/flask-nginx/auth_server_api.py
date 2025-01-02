78c93207ff878f9b6c6fe14b6fb91da45cf02bf6
Cameron Noupoue <cameronnoupoue@gmail.com>
2025-01-02 16:30:33 +0100

    editing script

--- ubuntu/ubuntu-client/flask-nginx/auth_server_api.py
+++ ubuntu/ubuntu-client/flask-nginx/auth_server_api.py
@@ -36,5 +36,5 @@ def index():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
 
--- ubuntu/ubuntu-client/runapp.sh
+++ ubuntu/ubuntu-client/runapp.sh
@@ -10,8 +10,16 @@ sudo docker compose -f docker-compose.yaml up -d
 
 echo "MongoDB up"
 
+echo "Stop Flask and Node Red"
+node-red stop
+pkill -f flask
+kill -9 $(lsof -t -i :1880)
+kill -9 $(lsof -t -i :5050)
+sleep 1
+
 echo "Lauching Flask API"
 python3 flask-nginx/auth_server_api.py &
+sleep 1
 
 echo "Launching Node Red"
-node-red start
+node-red &
