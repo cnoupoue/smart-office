57a9ef43e489ad6718e15f96ebb9e1bdb6557efd
Cameron Noupoue <cameronnoupoue@gmail.com>
2025-01-10 02:30:50 +0100

    secure flask with https

--- ubuntu/ubuntu-client/nginx/install.sh
+++ ubuntu/ubuntu-client/nginx/install.sh
@@ -7,7 +7,6 @@ sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo cp login.html /etc/nginx/html/login.html
 
 sudo openssl genrsa -out ../../../certificates/nginx.key 2048
-#sudo openssl genpkey -algorithm RSA -out ../../../certificates/nginx.key -aes256
 sudo openssl req -new -key ../../../certificates/nginx.key -out ../../../certificates/nginx.csr
 sudo openssl x509 -req -in ../../../certificates/nginx.csr -CA ../../../certificates/ca.crt -CAkey ../../../certificates/ca.key -CAcreateserial -out ../../../certificates/nginx.crt -days 365
 
